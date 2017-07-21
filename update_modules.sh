837c972e4b9cc0445bc6bf77e0acc96165e7192f
Alexander Semushev <savinwork@gmail.com>
2017-07-21 14:03:11 +0300

    add FFmpeg, openh264, update script

---
+++ update_modules.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "============================================"
+echo "cleanup modules"
+rm -rf ffmpeg
+rm -rf openh264
+
+echo "============================================"
+echo "Updating submodules"
+git submodule update --init || exit 1
+
+## or use openh264 v1.7.0 (tested on FFmpeg 3.3.2)
+# rm -rf openh264
+# wget -O- https://github.com/cisco/openh264/archive/v1.7.0.tar.gz | tar xz
+# mv openh264-1.7.0 openh264
+
+echo "Done!"
+echo "============================================"
+
+#$SHELL
